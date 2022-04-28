62d840d2afd360c5b4cfae106c1da123da269968
Al Kafi <alkafi28@gmail.com>
2022-04-29 00:20:01 +0600

    navbar added

---
+++ src/Components/HomePage/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../CommonComp/Footer';
+import Header from '../CommonComp/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h1>This is home</h1>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
