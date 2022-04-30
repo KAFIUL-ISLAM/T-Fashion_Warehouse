434023f88f0860cbba0bd4e973a282dac174b0dc
Al Kafi <alkafi28@gmail.com>
2022-04-30 13:38:49 +0600

    footer added

--- src/Components/CommonComp/Header.js
+++ src/Components/CommonComp/Header.js
@@ -20,21 +20,23 @@ const Header = () => {
                     <ul className="flex font-semibold justify-between">
                         {/*  Active Link = text-indigo-500 
                         Inactive Link = hover:text-indigo-500  */}
-                        <li className="md:px-4 md:py-2 text-indigo-500"><Link to={'/'}>Dashboard</Link></li>
-                        <li className="md:px-4 md:py-2 hover:text-indigo-400"><Link to={'/'}>Search</Link></li>
-                        <li className="md:px-4 md:py-2 hover:text-indigo-400"><Link to={'/'}>Explore</Link></li>
-                        <li className="md:px-4 md:py-2 hover:text-indigo-400"><Link to={'/'}>About</Link></li>
-                        <li className="md:px-4 md:py-2 hover:text-indigo-400"><Link to={'/'}>Contact</Link></li>
+                        <li className="md:px-4 md:py-2 text-indigo-500"><Link to={'/'}>Home</Link></li>
+                        <li className="md:px-4 md:py-2 hover:text-indigo-400"><Link to={'/'}>Add Items</Link></li>
+                        <li className="md:px-4 md:py-2 hover:text-indigo-400"><Link to={'/'}>Manage Inventories</Link></li>
+                        <li className="md:px-4 md:py-2 hover:text-indigo-400"><Link to={'/'}>Blogs</Link></li>
+                        <li className="md:px-4 md:py-2 hover:text-indigo-400"><Link to={'/'}>My Items</Link></li>
                     </ul>
                 </div>
                 <div className="order-2 md:order-3">
-                    <button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
-                        {/* Login Icon  */}
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clipRule="evenodd" />
-                        </svg>
-                        <span>Login</span>
-                    </button>
+                    <Link to={'/login'}>
+                        <button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2">
+                            {/* Login Icon  */}
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clipRule="evenodd" />
+                            </svg>
+                            <span>Login</span>
+                        </button>
+                    </Link>
                 </div>
             </div>
         </nav>
