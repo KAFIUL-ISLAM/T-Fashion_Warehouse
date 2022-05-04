97d889843755adc2ad34d64f2647ae6943a0bdea
Al Kafi <alkafi28@gmail.com>
2022-05-04 22:24:02 +0600

    update page builded

--- src/Components/HomePage/Item.js
+++ src/Components/HomePage/Item.js
@@ -1,9 +1,10 @@
 import aos from 'aos';
 import 'aos/dist/aos.css';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Item = props => {
-    const { name, price, quantity, supplier, description, image } = props.item;
+    const { _id, name, price, quantity, supplier, description, image } = props.item;
 
     aos.init();
 
@@ -27,7 +28,7 @@ const Item = props => {
     return (
         <div data-aos="fade-up" data-aos-duration="1000" data-aos-easing="ease-in-out" data-aos-delay="50" className="p-4 text-center cursor-pointer">
             <div className="py-8 rounded overflow-hidden shadow-lg hover:bg-white transition duration-500 bg-gray-100 text-slate-800">
-                <div className="space-y-10">
+                <div className="space-y-6">
                     <img className="mx-auto h-[350px] w-[320px] border-2 border-gray-400 rounded" src={image} alt="" />
 
                     <div className='flex justify-between px-8'>
@@ -36,7 +37,7 @@ const Item = props => {
                     </div>
 
                     <div className="px-6">
-                        <div className="space-y-5">
+                        <div className="space-y-3">
                             <div className="font-bold text-xl mb-2">{title}</div>
                             <p className="text-gray-700 text-base">
                                 {about}
@@ -44,6 +45,10 @@ const Item = props => {
                             <p>Supplier: <span className='text-orange-600 font-semibold'>{supplier}</span></p>
                         </div>
                     </div>
+                    <Link to={`/manage/${_id}`} className="text-white bg-indigo-500 hover:bg-indigo-600 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center">
+                        Update
+                        <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                    </Link>
                 </div>
             </div>
         </div>
--- src/Components/UpdateItem/UpdateItem.js
+++ src/Components/UpdateItem/UpdateItem.js
@@ -8,16 +8,31 @@ const UpdateItem = () => {
 
     const { id } = useParams();
     const [item] = useSingleItem(id);
-    
+    const { _id, name, price, quantity, supplier, description, image } = item;
+
     return (
         <div>
             <Header></Header>
-            <div className='grid grid-cols-3 min-h-screen'>
-                <div className=''>
-                    <h1>this is update panel</h1>
+            <div className='grid md:grid-cols-3 min-h-screen font-serif'>
+                <div className='text-center py-8'>
+                    <h1 className='font-bold text-4xl mb-8 text-slate-800'>Update Stock</h1>
+                    <p className='text-xl'>Available: <span className='text-red-600 text-4xl font-bold'>{quantity}</span></p>
                 </div>
                 <div className='col-span-2'>
-                    <h1>{item.name}</h1>
+                    <div className="py-8 text-slate-800">
+                        <div className="space-y-6">
+                            <div className="font-bold text-4xl mb-8 text-center">{name}</div>
+                            <img className="mx-auto" src={image} alt="" />
+
+                            <div className='space-y-10 px-16 text-xl font-bold'>
+                                <p>Price: <span className='text-blue-600  font-bold'>${price}</span></p>
+                                <p className="text-gray-600 text-base">
+                                    {description}
+                                </p>
+                                <p>Supplier: <span className='text-orange-600 font-semibold'>{supplier}</span></p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer></Footer>
